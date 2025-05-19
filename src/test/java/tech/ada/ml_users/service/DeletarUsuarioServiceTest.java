3efb9ecae3c6727d4bfd09781c3cd99c577826bc
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-19 19:49:27 -0300

    Cria teste deveLancarUmaExcecaoAoNaoEncontrarUmUsuario em DeletarUsuarioServiceTest

--- src/test/java/tech/ada/ml_users/service/DeletarUsuarioServiceTest.java
+++ src/test/java/tech/ada/ml_users/service/DeletarUsuarioServiceTest.java
@@ -1,10 +1,12 @@
 package tech.ada.ml_users.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
+import tech.ada.ml_users.exception.UsuarioNaoEncontradoException;
 import tech.ada.ml_users.repository.UsuariosRepository;
 
 class DeletarUsuarioServiceTest {
@@ -39,5 +41,20 @@ class DeletarUsuarioServiceTest {
         InOrder inOrder = Mockito.inOrder(buscarUsuariosService, repository);
         inOrder.verify(buscarUsuariosService).buscarUsuarioPorId(id);
         inOrder.verify(repository).deleteById(id);
+    }
+
+    @Test
+    void deveLancarUmaExcecaoAoNaoEncontrarUmUsuario(){
+        //Cenario
+        Long id = 1L;
+        Mockito.when(buscarUsuariosService.buscarUsuarioPorId(id)).thenThrow(new UsuarioNaoEncontradoException("Usuário com ID " + id + " não encontrado"));
+
+        //Execução
+        UsuarioNaoEncontradoException exception = Assertions.assertThrows(UsuarioNaoEncontradoException.class, () -> deletarUsuarioService.deletarUsuarioPorId(id));
+
+        //Verificação
+        Assertions.assertNotNull(exception);
+        Assertions.assertNotNull(exception.getMessage());
+
     }
 }
